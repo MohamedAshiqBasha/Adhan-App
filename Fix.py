e0a0db3d2059f6fcdfceb1acd5704f5c007f18a1
MohamedAshiqBasha <98243585+MohamedAshiqBasha@users.noreply.github.com>
2026-03-03 17:57:34 -0600

    Create Fix.py

---
+++ Fix.py
@@ -0,0 +1,9 @@
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
